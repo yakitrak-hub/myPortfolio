bfef42a68a39285b050856286f3fac0bdc983aa5
Kartikay Jain <yakitrak@gmail.com>
2020-12-30 15:18:54 +0400

    adding carousel, scrolldown

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from "react";
 
 //components
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from './components/title-message/title-message.component'
+
 import './App.css';
 
 const App = () => {
   return (
     <div>
       <MyNavbar />
-     My Portfolio
+      <MyCarousal/>
+      <TitleMessage/>
     </div>
   );
 }
